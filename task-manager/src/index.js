ea9f5d4dceee6990568469a54f4cce0d07b1d7a0
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2023-05-28 23:45:21 +0600

    feat: Implemented api authentication

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -16,3 +16,31 @@ app.use(taskRoutes);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+// const bcrypt = require('bcryptjs');
+
+// const myFunction = async () => {
+//   const password = "Red12345!";
+
+//   const hashedPassword = await bcrypt.hash(password, 8);
+  
+//   console.log("Password : ", password);
+//   console.log("Hashed Password : ", hashedPassword);
+
+//   const isMatch = await bcrypt.compare(password, hashedPassword);
+//   console.log("Is match : ", isMatch);
+// }
+
+// myFunction();
+
+// const jwt = require("jsonwebtoken");
+
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: "abc123" }, "thisismycourse", { expiresIn: "7 days" });
+//   console.log(token);
+
+//   const data = jwt.verify(token, "thisismycourse");
+//   console.log(data);
+// };
+
+// myFunction();
